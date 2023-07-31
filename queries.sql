5fff8d0293240cefaa539574af85dcdb99788d71
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-07-31 17:47:58 +0200

    fix

--- queries.sql
+++ queries.sql
@@ -12,21 +12,43 @@ SELECT * FROM `students` WHERE `date_of_birth` < '1993-%';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-SELECT * FROM `courses` WHERE `period` LIKE 'I semestre' AND `year` like 1;
+SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND HOUR(`hour`) >= 14;
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
-SELECT * FROM `degrees` WHERE `level` like 'magistrale';
-
+SELECT * FROM `degrees` WHERE `level` = 'magistrale';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
-SELECT * FROM `departments`
+SELECT COUNT(*) AS `numero dipartimenti` FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
-SELECT * FROM `teachers` WHERE `phone` is NULL;
+SELECT COUNT(`phone`) AS `numero insegnanti` FROM `teachers`;
+
+
+-----------------------------------------------------------------------------------------------------
+
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT COUNT(*) AS `numero studenti` , YEAR(`enrolment_date`) AS `anno` FROM `students` GROUP BY(`anno`);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(*) AS `num_insegnanti` , `office_address` AS `numero_ufficio` FROM `teachers` GROUP BY `office_number`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT `exam_id`, ROUND(AVG(`vote`)) AS `vote_average` FROM `exam_student` GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT COUNT(`name`) AS `Corso`, `department_id` FROM `degrees` GROUP BY `department_id`;
+
+
+------------------------------------------------------------------------------------------------------------
