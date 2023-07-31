20b144be72527b798521d383c91131164961ddb1
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-07-31 20:01:17 +0200

    J2

--- queries.sql
+++ queries.sql
@@ -65,7 +65,10 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
-
+SELECT `degrees` . `name` , `departments`.`name` 
+FROM `degrees`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
